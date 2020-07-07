00425dd3210c6d400a1af2e03a0c5f21d0cfd6df
MySister <448865418@qq.com>
2020-07-07 11:35:44 +0800

    第二次  修改     idea去提交项目

--- Git_one1/src/com/git/FirstGit.java
+++ Git_one1/src/com/git/FirstGit.java
@@ -2,6 +2,6 @@ package com.git;
 
 public class FirstGit {
     public static void main(String[] args) {
-        System.out.println("第一个Git11");
+        System.out.println("第一个Git1112");
     }
 }
